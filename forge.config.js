4ab1d49f03f9804c1656a74a52e9bb295eff652b
Zenin Easa Panthakkalakath <zeninpeasa@gmail.com>
2023-04-07 21:56:47 +0200

    Updating icon for electron-forge

--- forge.config.js
+++ forge.config.js
@@ -5,20 +5,37 @@ module.exports = {
     rebuildConfig: {},
     makers: [
         {
+            // For windows:
             name: '@electron-forge/maker-squirrel',
-            config: {},
+            config: {
+                iconUrl: 'https://raw.githubusercontent.com/zenineasa/HexHoot/main/modules/ImagePack/images/DesktopIcons/icon_windows.ico',
+                setupIcon: './modules/ImagePack/images/DesktopIcons/icon_windows.ico',
+            },
         },
         {
-            name: '@electron-forge/maker-zip',
-            platforms: ['darwin'],
+            // For mac:
+            name: '@electron-forge/maker-dmg',
+            config: {
+                icon: './modules/ImagePack/images/DesktopIcons/icon_mac.icns',
+            },
         },
         {
+            // For debian:
             name: '@electron-forge/maker-deb',
-            config: {},
+            config: {
+                options: {
+                    icon: './modules/ImagePack/images/DesktopIcons/icon_linux.png',
+                },
+            },
         },
         {
+            // For other linux:
             name: '@electron-forge/maker-rpm',
-            config: {},
+            config: {
+                options: {
+                    icon: './modules/ImagePack/images/DesktopIcons/icon_linux.png',
+                },
+            },
         },
     ],
 };
