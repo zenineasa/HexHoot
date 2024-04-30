8a667d8c3793cda7bea149f7e9e9cff04e1bb54b
Zenin Easa Panthakkalakath <zeninpeasa@gmail.com>
2024-04-30 22:53:44 +0400

    Removing Tone.js from dependencies list; updating external dependency versions

--- modules/Sounds/index.js
+++ modules/Sounds/index.js
@@ -1,6 +1,4 @@
-/* Copyright (c) 2022-2023 Zenin Easa Panthakkalakath */
-
-const Tone = require('tone');
+/* Copyright (c) 2022-2024 Zenin Easa Panthakkalakath */
 
 /**
  * This class implements the different sounds or tones used in the UI.
@@ -19,7 +17,8 @@ class Sounds {
      * Initialize Sounds
      */
     initialize() {
-        this.synth = new Tone.Synth().toDestination();
+        const AudioContext = window.AudioContext || window.webkitAudioContext;
+        this.audioContext = new AudioContext();
     }
 
     /**
@@ -29,11 +28,33 @@ class Sounds {
      */
     playFromArray(notes, duration) {
         const dt = duration / notes.length;
-        const now = Tone.now();
+        const startTime = this.audioContext.currentTime;
         for (let i = 0; i < notes.length; i++) {
-            this.synth.triggerAttackRelease(
-                notes[i], '16n', now + i * dt);
+            const oscillator = this.audioContext.createOscillator();
+            oscillator.connect(this.audioContext.destination);
+            oscillator.frequency.setValueAtTime(
+                this.noteToFrequency(notes[i]),
+                startTime + i * dt
+            );
+            oscillator.start(startTime + i * dt);
+            oscillator.stop(startTime + (i + 1) * dt);
         }
+    }
+
+    /**
+     * Mapping frequency to notes
+     */
+    noteToFrequency(note) {
+        const notesMap = {
+            'C4': 261.63,
+            'D4': 293.66,
+            'E4': 329.63,
+            'F4': 349.23,
+            'G4': 392.00,
+            'A4': 440.00,
+            'B4': 493.88
+        };
+        return notesMap[note] || 0;
     }
 
     /**
